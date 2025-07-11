3319a373a67e35133af99e5f087727f7cdcc137f
Harkirat Singh Kaur <khalsaharkirat97@gmail.com>
2025-07-11 20:46:31 +0200

    feat: creado el esqueleto con estilo de la pagina principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { inter } from "./ui/fonts"
 import "./globals.css";
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap'
-})
-
 
 export const metadata: Metadata = {
   title: "Economic Metrics",
@@ -20,11 +13,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es" className={`${inter.variable} scroll-smooth`}>
       <body
-        className={`${inter.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
-        {children}
+        <main className="min-h-screen w-full flex flex-col items-center">
+          <header className="w-full flex justify-between items-center px-10 py-8">
+            <h1 className="text-3xl font-bold tracking-tight">Datos</h1>
+            <nav className="space-x-6 text-sm text-foreground">
+              <a href="#" className="hover:underline">Indicadores</a>
+              <a href="#" className="hover:underline">Países</a>
+              <a href="#" className="hover:underline">Categorías</a>
+            </nav>
+          </header>
+          {children}
+        </main>
       </body>
     </html>
   );
